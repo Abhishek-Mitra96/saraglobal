8119504f3caa003cda90a9eef250e7dc3c63d546
utsab chatterjee <utsab@goyalsoftwares.com>
2017-06-23 11:12:11 +0530

    footer quick links

--- footer.php
+++ footer.php
@@ -1,11 +1,19 @@
 <!-- footer start-->
+<style>
+.quick_links_hr 
+{
+    padding: 5px;
+    margin: 0px;
+    color:#000000;
+}
+</style>
     <footer class="cs-main-footer">
         <div class="container">
             <div class="row">
                 <div class="col-lg-4 col-md-4 col-sm-4">
                     <div class="cs-footer-col">
                         <h2 style="color: #DEB887;"> SARA GLOBAL</h2>
-                        <p style="font-size: 12px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud cillum dolore eu fugiat nulla pariatur. </p><br/>
+                            <p style="font-size: 12px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud cillum dolore eu fugiat nulla pariatur. </p><br/>
 
                         <p style="font-size: 12px;">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                     </div>
@@ -13,18 +21,12 @@
                 <div class="col-lg-4 col-md-4 col-sm-4">
                     <div class="cs-footer-col centered">
                         <h2 style="color: #DEB887;"> QUICK LINKS</h2>
-                        <a href="index.php">HOME</a><hr style="padding: 5px;
-    margin: 0px; color:#000000;">
-                        <a href="about-1.php">ABOUT US</a><hr style="padding: 5px;
-    margin: 0px; color:#000000;">
-                        <a href="#">SERVICES</a><hrstyle="padding: 5px;
-    margin: 0px; color:#000000;">
-                        <a href="#">PORTFOLIO</a><hr style="padding: 5px;
-    margin: 0px; color:#000000;">
-                        <a href="#">CLIENTS</a><hr style="padding: 5px;
-    margin: 0px; color:#000000;">
-                        <a href="#">CONTACT US</a><hr style="padding: 5px;
-    margin: 0px; color:#000000;">
+                        <a href="index.php">HOME</a><hr class="quick_links_hr">
+                        <a href="about-1.php">ABOUT US</a><hr class="quick_links_hr">
+                        <a href="service-1.php">SERVICES</a><hr class="quick_links_hr">
+                        <a href="portfolio.php">PORTFOLIO</a><hr class="quick_links_hr">
+                            <a href="clients.php">CLIENTS</a><hr class="quick_links_hr">
+                        <a href="contact-1.php">CONTACT US</a><hr class="quick_links_hr">
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-4 col-sm-4">
